dc28519ffc54edc0bfb53f7e880789a37abeba1d
Belian Tomasz <bel.tomasz@gmail.com>
2020-06-06 10:42:34 +0200

    LED Matrix Libary hinzugefügt

--- Tetris.ino
+++ Tetris.ino
@@ -2,7 +2,25 @@
   Erste Tests mit einem 2D Array und einer Seriellen ausgabe
 */
 
+//LED Matrix Libary einbinden
+#include <RGBmatrixPanel.h>
 
+
+//Pins für die LED Matrix
+#define CLK  8   // USE THIS ON ADAFRUIT METRO M0, etc.
+//#define CLK A4 // USE THIS ON METRO M4 (not M0)
+//#define CLK 11 // USE THIS ON ARDUINO MEGA
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
+
+
+//Globale Variablen
 int myNumbers[10][15];
 bool newBlock = true;
 int xLength = 10;
@@ -18,6 +36,11 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
 
+
+  matrix.begin();
+
+
+
 }
 
 // the loop function runs over and over again forever
@@ -30,10 +53,10 @@ void loop() {
   */
 
   //überprüfen, ob der Block in der nächsten Position an einem anderen ankommt, oder am Boden
-  if (yBlock + 1 >= yLength || myNumbers[xBlock][yBlock+1] > 1) {
+  if (yBlock + 1 >= yLength || myNumbers[xBlock][yBlock + 1] > 1) {
     Serial.println("Fix Block");
 
-    
+
     //Aktuellen Block fix setzen
     for (byte i = 0; i < xLength; i = i + 1) {
 
@@ -74,16 +97,27 @@ void loop() {
 
 
 
+  //Spielfeld über die Serielle schnittstelle ausgeben
+  serialPrintSpielfeld();
+
+  delay(1000);
+}
 
 
 
 
+//------------------------  Methoden   ---------------------
 
 
+void newblock() {//Einen Neuen Block setzen
 
+  blockColor++;
 
+  yBlock = 0;
+  newBlock = true;
+}
 
-
+void serialPrintSpielfeld() { //Spielfeld über die Serielle schnittstelle ausgeben
 
   for (byte i = 0; i < xLength; i = i + 1) {
 
@@ -94,23 +128,5 @@ void loop() {
     Serial.println("");
   }
 
-
-
-
-
   Serial.println("-----");
-
-  delay(1000);
-}
-
-
-
-void newblock() {
-
-  blockColor++;
-
-  yBlock = 0;
-  newBlock = true;
-
-
 }
